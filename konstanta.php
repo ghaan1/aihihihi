314a732c898585b3abdadcac219fdbc1b9d0b551
ghaan1 <gono0608@gmail.com>
2021-12-10 10:19:15 +0700

    first commit

---
+++ konstanta.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+            $var = "ini adalah contoh variabel";
+            echo $var;
+            echo "<br><br>";
+            define('KONSTANTA', "ini adalah contoh konstanta");
+            echo KONSTANTA;
+            echo "<br>";
+            echo KONSTANTA;
+        ?>
+    </body>
+</html>
---
+++ tipedata.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+            $a = "Hello World!";
+            $b = 99;
+            $c = 6.8;
+            $d = null;
+            var_dump ($a,$b,$c,$d);
+        ?>
+    </body>
+</html> 
